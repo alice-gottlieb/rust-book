4f49edd05664c6bab6f35c0fd78158d35e36bf9e
alice-gottlieb <aliceagottlieb@gmail.com>
2023-07-01 14:26:22 -0700

    Generate random number and respond with yes/no

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
@@ -12,4 +15,15 @@ fn main() {
         .expect("Should have read line from console.");
 
     println!("You guessed: {guess}");
+
+    match guess.trim().parse::<u32>() {
+        Ok(num) => {
+            if num == secret_number {
+                println!("You win!");
+            } else {
+                println!("You lose!, The secret number was: {secret_number}");
+            }
+        },
+        Err(_) => println!("You didn't enter a number."),
+    }
 }
